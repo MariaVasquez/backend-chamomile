4f573fea7001c72956a4104ee16c839c1248fff8
Maria Vasquez <mfvasquez17@outlook.com>
2024-04-19 22:40:33 -0500

    se inicia proyecto user

---
+++ src/main/java/org/user/api/userchamomile/UserChamomileApplication.java
@@ -0,0 +1,13 @@
+package org.user.api.userchamomile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserChamomileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserChamomileApplication.class, args);
+    }
+
+}
